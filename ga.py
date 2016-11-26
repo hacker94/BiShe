803144ed9a85739de5533b1643be85e5797385f9
hacker94 <87306223@163.com>
2016-11-27 01:01:36 +0800

    'leastsq'

--- ga.py
+++ ga.py
@@ -6,14 +6,12 @@ Created on Wed Sep 14 23:41:26 2016
 """
 
 import random
+import csv
+import numpy as np
+from scipy.optimize import leastsq
 
 class Population:
     
-    ##
-    # @usage => constructor
-    # @para0 => int, population size
-    # @para1 => bool, initializing
-    ##
     def __init__(self, population_size, need_init = True):
         self.individuals = [] # store individuals of this population
         if need_init:
@@ -22,40 +20,18 @@ class Population:
                 individual.generate_individual()
                 self.save_individual(individual)
     
-    ##
-    # @usage => get an individual by index
-    # @para0 => int, individual index
-    # @return => Individual, the individual
-    ##
     def get_individual(self, index):
         return self.individuals[index]
         
-    ##
-    # @usage => get an iter of all individuals
-    # @return => listiterator, the iter of all individuals
-    ##
     def get_individuals(self):
         return iter(self.individuals)
         
-    ##
-    # @usage => save an individual on index
-    # @para0 => int, index
-    # @para0 => Individual, individual 
-    ##
     def save_individual(self, individual):
         self.individuals.append(individual)
-
-    ##
-    # @usage => get the size of this population
-    # @return => int, size
-    ##        
+  
     def size(self):
         return len(self.individuals)
-        
-    ##
-    # @usage => get the fittest individual in this population
-    # @return => the fittest individual
-    ##
+
     def get_fittest(self):
         if self.size() == 0:
             return None
@@ -65,7 +41,6 @@ class Population:
             if individual.get_fitness() > fittest.get_fitness():
                 fittest = individual
         return fittest
-    
     
     
 class Individual:
@@ -104,7 +79,6 @@ class Individual:
         return ''.join([chr(x + ord('0')) for x in self.genes])
         
 
-
 class Algorithm:
     
     uniform_rate = 0.5
@@ -128,7 +102,7 @@ class Algorithm:
             new_indiv = Algorithm.crossover(indiv1, indiv2)
             new_pop.save_individual(new_indiv)
         
-        for indiv in new_pop.get_individuals(): 
+        for indiv in new_pop.get_individuals()[(1 if Algorithm.elitism else 0):]: 
             Algorithm.mutate(indiv)
         
         return new_pop
@@ -185,8 +159,94 @@ class FitnessCalc:
     def get_max_fitness():
         return len(FitnessCalc.solution)
         
+
+
+def read():
+    with open('DAX-price-2-years.csv', 'rb') as price_csv, open('DAX-sentiment-2-years.csv', 'rb') as sentiment_csv:
+        price_reader = csv.reader(price_csv)
+        sentiment_reader = csv.reader(sentiment_csv)
+        
+        trends = {}
+        last_price = 0
+        for row in price_reader:
+            price = float(row[4])
+            date = row[0]
+            if last_price != 0:
+                trends[date] = 1 if price > last_price else 0
+            last_price = price
+        m = len(trends.keys()) # total data
+        
+        sentiments = {}
+        event_id = {}
+        
+        event_count = {}
+        
+        n = 0 # total characteristics
+        for row in sentiment_reader:
+            date = row[0]
+            event = (int(row[1]), int(row[2]), int(row[3]))
+            if not event in event_id:
+                event_id[event] = n
+                n += 1
+                
+                event_count[event] = 1
+            else:
+                event_count[event] += 1
+                
+            weight = int(row[4])
+            if not date in sentiments:
+                sentiments[date] = {}
+            sentiments[date][event] = weight
+            
+    i = 0
+    for event in event_count.keys():
+        if event_count[event] < 200:
+            event_id[event] = -1
+            n -= 1
+        else:
+            event_id[event] = i
+            i += 1
+    
+    return trends, sentiments, event_id, m, n
+    
+def build_vectors(trends, sentiments, event_id, m, n):
+    X_all = np.zeros((m, n))
+    y_all = np.zeros(m)
+    
+    dates = sorted(trends.keys())
+    for i in xrange(0, m):
+        date = dates[i]
+        for event in sentiments[date]:
+            if event_id[event] != -1:
+                X_all[i, event_id[event]] = sentiments[date][event]
+            #X_all[i, event_id[event]] = sentiments[date][event]
+        y_all[i] = trends[date]
+    
+    return X_all, y_all
+        
+def h(theta, x):
+    return 1 / (1 + np.exp(-np.dot(x, theta)))  # x can be a vector or a matrix
+    
+def residuals(theta, X, y):
+    return h(theta, X) - y
+    
+def train(X, y):
+    theta = np.zeros(X.shape[1])
+    theta = leastsq(residuals, theta, args = (X, y))[0]
+    return theta
+    
+def test(X, y, theta):
+    total = y.shape[0]
+    correct = 0
+    for i in xrange(total):
+        yh = h(theta, X[i])
+        yh = 1 if yh > 0.5 else 0
+        if yh == y[i]:
+            correct += 1
+    print 'Total: %d, Correct: %d, Accuracy: %f' % (correct, total, float(correct) / total)
         
 if __name__ == '__main__':
+    '''
     FitnessCalc.set_solution('1111000000000000000000000000000000000000000000000000000000001111')
     my_pop = Population(50)
     generation_cnt = 0
@@ -198,4 +258,22 @@ if __name__ == '__main__':
     print 'Solution found!'
     print 'Generation: %d' % generation_cnt
     print 'Genes: %s' % my_pop.get_fittest()
+    '''
+    training_num = 400
+
+    # read trends and sentiments from file
+    trends, sentiments, event_id, m, n = read()
+    
+    # build vectors
+    X_all, y_all = build_vectors(trends, sentiments, event_id, m, n)
         
+    # work out theta
+    X = X_all[0:training_num]
+    y = y_all[0:training_num]
+    theta = train(X, y)
+    
+    # test
+    X_test = X_all[training_num:]
+    y_test = y_all[training_num:]
+    test(X_test, y_test, theta)
+    
